8bad08a996fe09a55a998ae092c37d9f5c37c728
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-07 12:37:26 -0600

    add items for merchant test

---
+++ spec/requests/merchant_item_request_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "get all items for a merchant" do
+  it 'gets all items associated with a merchant' do
+    merchant = create(:merchant)
+    create_list(:item, 21, merchant: merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items).to be_a(Hash)
+    check_hash_structure(items, :data, Array)
+    expect(items[:data].count).to eq(21)
+
+    items[:data].each do |item|
+      expect(item).to be_a(Hash)
+      check_hash_structure(item, :id, String)
+      check_hash_structure(item, :type, String)
+      check_hash_structure(item, :attributes, Hash)
+      check_hash_structure(item[:attributes], :name, String)
+      check_hash_structure(item[:attributes], :description, String)
+      check_hash_structure(item[:attributes], :unit_price, Float)
+      check_hash_structure(item[:attributes], :merchant_id, Integer)
+      expect(item.keys).to match_array(%i[id type attributes])
+      expect(item[:attributes].keys).to match_array(%i[name description unit_price merchant_id])
+    end
+  end
+
+  it 'returns an empty array if the merchant has no items' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items[:data]).to be_an(Array)
+    expect(items[:data].count).to eq(0)
+  end
+end
