1aa55e1a0a7a0fd9a883a64beb63ad5ab300c466
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-07 12:18:45 -0600

    add merchant show tests

---
+++ spec/requests/merchant_show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'get one merchant' do
+  it 'returns a single record by id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant).to be_a(Hash)
+    check_hash_structure(merchant, :data, Hash)
+    check_hash_structure(merchant[:data], :id, String)
+    check_hash_structure(merchant[:data], :type, String)
+    check_hash_structure(merchant[:data], :attributes, Hash)
+    check_hash_structure(merchant[:data][:attributes], :name, String)
+    expect(merchant[:data].keys).to match_array(%i[id type attributes])
+    expect(merchant[:data][:attributes].keys).to match_array(%i[name])
+  end
+end
