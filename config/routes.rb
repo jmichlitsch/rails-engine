93943eb7462e5cb6ae257a47c9e9a80a21305034
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-08 21:22:49 -0600

    add routes to fix postman tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    namespace :v1 do
      get 'merchants/find', to: 'merchants#find'
      get 'merchants/find_all', to: 'merchants#find_all'
+     get 'merchants/most_items', to: 'merchants#most_items'
      resources :merchants, only: [:index, :show] do
        resources :items, only: :index
      end
@@ -12,6 +13,13 @@ Rails.application.routes.draw do
      resources :items do
         resource :merchant, only: :show
       end
+
+      get 'revenue', to: 'revenue#revenue'
+      get 'revenue/items', to: 'revenue#items'
+      get 'revenue/unshipped', to: 'revenue#unshipped'
+      get 'revenue/weekly', to: 'revenue#weekly'
+      get 'revenue/merchants', to: 'revenue#merchants'
+      get 'revenue/merchants/:id', to: 'revenue#merchant_revenue'
    end
  end
 end
