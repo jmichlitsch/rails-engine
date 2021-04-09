2aad95044b8ae3174146b6632c4e6cc7fd490598
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-08 20:07:09 -0600

    add tests for top merchants

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -14,7 +14,8 @@ RSpec.describe Merchant, type: :model do
       expect(selected.to_a.size).to eq(20)
     end
    end
-   describe '.find_one_by_name' do
+
+  describe '.find_one_by_name' do
     it 'finds a merchant using a search term' do
       merchant = create(:merchant, name: "Ring World")
       create(:merchant, name: "Bob's Burgers")
@@ -22,5 +23,29 @@ RSpec.describe Merchant, type: :model do
       expect(Merchant.find_one_by_name('ring')).to eq(merchant)
     end
   end
+
+  describe '.top_merchants' do
+      before(:each) do
+        6.times do |n|
+          merchant = create(:merchant)
+          (n + 1).times do
+            item = create(:item, merchant: merchant)
+            invoice = create(:invoice, merchant: merchant, status: 'shipped')
+            create(:invoice_item, invoice: invoice, item: item, unit_price: 1.0, quantity: 1)
+            create(:transaction, invoice: invoice, result: 'success')
+          end
+        end
+      end
+
+      it 'finds a specified quantity of merchants' do
+        merchants = Merchant.top_merchants(5)
+
+        expect(merchants.to_a.size).to eq(5)
+        expect(merchants[0].revenue).to eq(6)
+        expect(merchants[1].revenue).to eq(5)
+        expect(merchants[2].revenue).to eq(4)
+        expect(merchants[3].revenue).to eq(3)
+        expect(merchants[4].revenue).to eq(2)
+      end
  end
 end
