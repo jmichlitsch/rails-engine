0a4421f23e34f8d69345178cbc9222dbd1bdeadd
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-08 20:52:11 -0600

    add methods for invoice item active record

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,14 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  def self.total_revenue_by_date(start, end_date)
+    where(invoices: { created_at: Date.parse(start)..Date.parse(end_date).end_of_day }).total_revenue
+  end
+
+  def self.total_revenue
+    joins(:invoice)
+      .merge(Invoice.completed)
+      .sum('quantity * invoice_items.unit_price')
+  end
 end
